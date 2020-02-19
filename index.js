b8e1c9196343ac2222c7c7dc388e8f5a6c336841
unknown <mrehawk82@gmail.com>
2020-02-19 10:32:05 -0500

    ...

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var scp = require('./scp_lookup');
+var scp = require('scp_lookup');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(scp.lookup());
-  //res.write(screamer.screamer(" helloWorld! "));
+  res.write(scp.lookup(3));
   res.end();
 }).listen(8080);
